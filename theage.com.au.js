d5be27f0b706f4f4f06d3e2cfe1a48282b75c05b
Pat Allan <pat@freelancing-gods.com>
2011-06-30 12:33:03 +1000

    Hiding a lot more rubbish. Yes, I should just read abc.net.au/news instead.

--- theage.com.au.js
+++ theage.com.au.js
@@ -1,1 +1,6 @@
 $('.ad').remove();
+$('.c5.classifieds.cfix').remove();
+$('.fN-affiliateStrip').remove();
+$('.strapHeadingDealPartner, .cN-storyDeal').remove();
+$('.cT-featuredNews').remove();
+$('.fN-mostViewed').remove();
